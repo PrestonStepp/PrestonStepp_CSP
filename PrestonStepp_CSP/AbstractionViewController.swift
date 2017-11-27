4c23aac3e58ef25234960a1d13929caabd5192dc
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-11-27 08:19:42 -0700

    added code

--- PrestonStepp_CSP/AbstractionViewController.swift
+++ PrestonStepp_CSP/AbstractionViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+{
+    //MARK: Array of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper method to retrieve the correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+}
