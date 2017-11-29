133be19051edecc7e2cdb80a983038e02beb4206
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-11-29 08:00:25 -0700

    Added code

--- PrestonStepp_CSP/TheInternetViewController.swift
+++ PrestonStepp_CSP/TheInternetViewController.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class TheInternetController: UITableViewConroller
+{
+    private
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do ant additional setup after loading the view
+        self.clearSelecrionViewWillAppear = false
+        
+        
+    }
+    //MARK: - Tabe view data source
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        
+    }
+}
